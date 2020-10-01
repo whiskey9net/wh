c3bb29141c6e692e084380d18b32febc073cc824
Whyskey9 <whiskey9net@gmail.com>
2020-10-01 18:38:15 +0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wh
 Project "Warehouse". Use it on your own risk!!
+
+#### Quick fix
+
+````
+git add -A; git commit -m "up"; git push
+````
---
+++ builds/runner1/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "**** Starting: [runner1] ****"
+echo
+echo "--- ENV ---------------------"
+echo "listening_ip: ${listening_ip}"
+echo "-----------------------------"
+
+sleep infinity
